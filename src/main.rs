02ff0dc9bf84a0eefd42e5a6024eaaf89a8ad6fa
Callum Jones <callum.o.jones@protonmail.com>
2023-06-13 11:48:29 +1000

    Move input handling, add round record and ui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io, time::{Instant, Duration}};
+use std::{
+    io::{self, LineWriter},
+    time::{Duration, Instant},
+};
 
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
@@ -11,11 +14,12 @@ use ratatui::{
     backend::{Backend, CrosstermBackend},
     layout::{Alignment, Constraint, Direction, Layout, Margin},
     style::{Modifier, Style},
-    text::{Span, Spans},
+    text::{Line, Span, Spans},
     widgets::{Block, Borders, Paragraph, Wrap},
     Frame, Terminal,
 };
 
+#[derive(Clone)]
 struct GameChar {
     expected_char: char,
     given_char: Option<char>,
@@ -27,6 +31,7 @@ enum CharStatus {
     Empty,
 }
 
+#[derive(Clone)]
 struct GameString {
     game_string: Vec<GameChar>,
     curr_index: usize,
@@ -105,85 +110,116 @@ impl GameString {
     }
 }
 
+#[derive(Clone)]
 struct Round {
     text: GameString,
     start_time: Instant,
-    end_time: Option<Instant>
+    end_time: Option<Instant>,
 }
 
 impl Round {
     pub fn new(target_str: String) -> Self {
-        Round { text: GameString::from(target_str) , start_time: Instant::now(), end_time: None }
+        Round {
+            text: GameString::from(target_str),
+            start_time: Instant::now(),
+            end_time: None,
+        }
+    }
+
+    pub fn calculate_wpm(&self) -> i32 {
+        90
+    }
+
+    pub fn is_complete(&self) -> bool {
+        match self.end_time {
+            Some(_) => true,
+            None => false,
+        }
+    }
+
+    pub fn handle_input(&mut self, key: &KeyEvent) {
+        match key.code {
+            KeyCode::Char(typed) => {
+                self.text.update_next_char(typed);
+                if self.text.is_completed() {
+                    self.end_time = Some(Instant::now());
+                }
+            }
+            KeyCode::Backspace => {
+                self.text.pop_char();
+            }
+            _ => {}
+        }
     }
 }
 
 enum GameStatus {
     Waiting,
     Ongoing(Round),
-    Complete
-}
-struct Game {
-    status: GameStatus
+    Complete,
 }
 
+struct Game {
+    status: GameStatus,
+    record: Vec<Round>,
+}
 
 impl Game {
-    pub fn new(time_lmt: i32) -> Self {
+    pub fn new() -> Self {
         Game {
-            status: GameStatus::Waiting
+            status: GameStatus::Waiting,
+            record: Vec::new(),
         }
     }
 
     pub fn elapsed_time(&self) -> Duration {
         match &self.status {
             GameStatus::Waiting => Duration::ZERO,
-            GameStatus::Ongoing(round) => {
-                round.start_time.elapsed()
-            },
+            GameStatus::Ongoing(round) => round.start_time.elapsed(),
             GameStatus::Complete => Duration::ZERO,
         }
     }
 
     pub fn handle_input(&mut self, key: KeyEvent) -> bool {
+        // Check for exit
         match key.code {
-            KeyCode::Esc => {
-                return true
-            },
+            KeyCode::Esc => return true,
             _ => {}
         }
-        
+
+        // Handle controls through the round state.
+        let mut finished_round = None;
         match &mut self.status {
             GameStatus::Waiting => {
                 // Enter the letter given and start the game
                 match key.code {
                     KeyCode::Enter => {
-                        self.status = GameStatus::Ongoing(Round::new(lipsum(20)));
-                    },
+                        self.status = GameStatus::Ongoing(Round::new(lipsum(5)));
+                    }
                     _ => {}
                 }
-            },
+            }
             GameStatus::Ongoing(round) => {
-                match key.code {
-                    KeyCode::Char(typed) => {
-                        round.text.update_next_char(typed);
-                        if round.text.is_completed() {
-                            self.status = GameStatus::Complete;
-                        }
-                    }
-                    KeyCode::Backspace => {
-                        round.text.pop_char();
-                    }
-                    _ => {}
+                round.handle_input(&key);
+                if round.end_time.is_some() {
+                    finished_round = Some(round.clone());
                 }
-            },
-            GameStatus::Complete => {
-                match key.code {
-                    KeyCode::Enter => {
-                        self.status = GameStatus::Waiting;
-                    },
-                    _ => {}
+            }
+            GameStatus::Complete => match key.code {
+                KeyCode::Enter => {
+                    self.status = GameStatus::Waiting;
                 }
+                _ => {}
             },
+        }
+
+        // Update GameState if necessary
+        match finished_round {
+            Some(round) => {
+                self.status = GameStatus::Complete;
+                self.record.push(round);
+            }
+            None => {}
         }
 
         false
@@ -200,18 +236,32 @@ fn ui<B: Backend>(f: &mut Frame<B>, game: &mut Game) {
     // let prompt_text = Span::raw(&game.target_phrase);
     match &game.status {
         GameStatus::Waiting => {
-            let waiting_msg = "Waiting... Press Enter to begin the timer and start a new round";
-            let prompt_box = Paragraph::new(Spans::from(waiting_msg))
-            .block(Block::default().title(" Waiting for new round... ").borders(Borders::ALL))
-            .style(
-                Style::default()
-                    .fg(ratatui::style::Color::White)
-                    .bg(ratatui::style::Color::Black),
-            )
-            .wrap(Wrap { trim: true })
-            .alignment(Alignment::Center);
-            f.render_widget(prompt_box, chunks[0]);
-        },
+            let waiting_msg = " 🏎️🏎️🏎️🏎️🏎️🏎️🏎️🏎️🏎️  ";
+            let title_box = Paragraph::new(Spans::from(waiting_msg))
+                .block(
+                    Block::default()
+                        .title(" 🏎️ Terminal Typer 🏎️ ")
+                        .borders(Borders::ALL),
+                )
+                .style(
+                    Style::default()
+                        .fg(ratatui::style::Color::White)
+                        .bg(ratatui::style::Color::Black),
+                )
+                .wrap(Wrap { trim: true })
+                .alignment(Alignment::Center);
+            f.render_widget(title_box, chunks[0]);
+
+            let prompt_msg = " [Enter]: New Game \n [Esc]: Exit Game";
+            let prompt_box = Paragraph::new(prompt_msg)
+                .block(Block::default().title(" Controls ").borders(Borders::ALL))
+                .style(
+                    Style::default()
+                        .bg(ratatui::style::Color::Black)
+                        .fg(ratatui::style::Color::White),
+                );
+            f.render_widget(prompt_box, chunks[1]);
+        }
         GameStatus::Ongoing(round) => {
             let mut rendered_text = Vec::new();
             for i in 0..round.text.game_string.len() {
@@ -220,7 +270,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, game: &mut Game) {
                     CharStatus::Incorrect => Style::default().fg(ratatui::style::Color::Red),
                     CharStatus::Empty => Style::default().fg(ratatui::style::Color::Gray),
                 };
-        
+
                 // TODO abstract this functionality into the class. Fine here for now.
                 let rendered_char = match round.text.game_string[i].given_char {
                     Some(c) => {
@@ -229,42 +279,77 @@ fn ui<B: Backend>(f: &mut Frame<B>, game: &mut Game) {
                         } else {
                             '·'
                         }
-                    },
+                    }
                     None => round.text.game_string[i].expected_char,
                 };
                 rendered_text.push(Span::styled(rendered_char.to_string(), style));
             }
 
             let prompt_box = Paragraph::new(Spans::from(rendered_text))
-                    .block(Block::default().title(" Prompt ").borders(Borders::ALL))
-                    .style(
-                        Style::default()
-                            .fg(ratatui::style::Color::White)
-                            .bg(ratatui::style::Color::Black),
-                    )
-                    .wrap(Wrap { trim: true })
-                    .alignment(Alignment::Center);
-                f.render_widget(prompt_box, chunks[0]);
+                .block(Block::default().title(" Prompt ").borders(Borders::ALL))
+                .style(
+                    Style::default()
+                        .fg(ratatui::style::Color::White)
+                        .bg(ratatui::style::Color::Black),
+                )
+                .wrap(Wrap { trim: true })
+                .alignment(Alignment::Center);
+            f.render_widget(prompt_box, chunks[0]);
 
-            let accuracy = format!("Word Accuracy: {}% \t \t Time Elapsed: {:?}", round.text.percentage_correct(), game.elapsed_time());
+            let accuracy = format!(
+                "Word Accuracy: {}% \t \t Time Elapsed: {:?}",
+                round.text.percentage_correct(),
+                game.elapsed_time()
+            );
             let block2 = Paragraph::new(accuracy)
                 .block(Block::default().title("Stats").borders(Borders::ALL))
                 .style(Style::default());
             f.render_widget(block2, chunks[1]);
-        },
+        }
         GameStatus::Complete => {
-            let waiting_msg = "Round complete! Press Enter to start a new round :)";
-            let prompt_box = Paragraph::new(Spans::from(waiting_msg))
-            .block(Block::default().title(" Round Complete ").borders(Borders::ALL))
-            .style(
-                Style::default()
-                    .fg(ratatui::style::Color::White)
-                    .bg(ratatui::style::Color::Black),
-            )
-            .wrap(Wrap { trim: true })
-            .alignment(Alignment::Center);
+            let waiting_msg = "Previous rounds:";
+            let mut lines = vec![Line::from(Span::raw(waiting_msg))];
+            for (index, round) in game.record.iter().enumerate() {
+                let curr_count = index + 1;
+                let new_line = Line::from(vec![
+                    Span::styled("Round ", Style::default().fg(ratatui::style::Color::Green)),
+                    Span::styled(
+                        curr_count.to_string(),
+                        Style::default().fg(ratatui::style::Color::Green),
+                    ),
+                    Span::raw(": "),
+                    Span::raw(round.text.percentage_correct().to_string()),
+                    Span::raw("% word accuracy, "),
+                    Span::raw(round.calculate_wpm().to_string()),
+                    Span::raw(" wpm"),
+                ]);
+                lines.push(new_line);
+            }
+            let prompt_box = Paragraph::new(lines)
+                .block(
+                    Block::default()
+                        .title(" Leaderboard ")
+                        .borders(Borders::ALL),
+                )
+                .style(
+                    Style::default()
+                        .fg(ratatui::style::Color::White)
+                        .bg(ratatui::style::Color::Black),
+                )
+                .wrap(Wrap { trim: true })
+                .alignment(Alignment::Center);
             f.render_widget(prompt_box, chunks[0]);
-        },
+
+            let prompt_msg = " [Enter]: New Game \n [Esc]: Exit Game";
+            let prompt_box = Paragraph::new(prompt_msg)
+                .block(Block::default().title(" Controls ").borders(Borders::ALL))
+                .style(
+                    Style::default()
+                        .bg(ratatui::style::Color::Black)
+                        .fg(ratatui::style::Color::White),
+                );
+            f.render_widget(prompt_box, chunks[1]);
+        }
     }
 }
 
@@ -276,7 +361,7 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
 
     // let target_str = String::from("This is the string we have to try to type. Good luck!");
-    let mut game = Game::new(100);
+    let mut game = Game::new();
 
     loop {
         terminal.draw(|f| {
